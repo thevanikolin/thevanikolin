3daf572eeb480027af162cb08d426fd1c4175a69
nikolin <136066503+thevanikolin@users.noreply.github.com>
2023-06-09 14:07:39 +0530

    Add files via upload

---
+++ exercise.py
@@ -0,0 +1,9 @@
+print('HELLO,World!')
+person=input('what is your name?')
+print('Hello',person)
+age=12
+print(age)
+weight=27
+print(weight)
+Height=110
+print(Height)
---
+++ myfirstprogram.py
@@ -0,0 +1,5 @@
+print('Hello, world!')
+person=(input('what is your name?'))
+print('hello',person)
+age=12
+print('what is your age?',age)
